a81da096b4e10c271276607a9d68644c0e077627
kiki.fuji <fujitakikuko@fujitakikuko-no-MacBook-Air.local>
2017-09-23 21:17:31 +0900

    カリキュラム分完成

---
+++ taskapp2/task.swift
@@ -0,0 +1,42 @@
+//
+//  task.swift
+//  taskapp2
+//
+//  Created by 藤田貴久子 on 2017/09/23.
+//  Copyright © 2017年 kiki.fuji. All rights reserved.
+//
+
+import Foundation
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用　ID プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    // 日時
+    dynamic var date = NSDate()
+
+    /**
+    id　をプラマリーキーとして設定
+    */
+ 
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+
+
+    
+    
+}
+
+
+
+
+
